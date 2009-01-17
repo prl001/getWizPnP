ac792c54373935af0164204adaaccf1dbe365aab
prl <prl@cae3c895-535b-42a1-8135-9dd7b94a49ff>
2009-01-17 21:29:01 +0000

    Allow configuration of maxdevs, number of devices expected to reply, search timeout and number of search polls as optional arguments.

--- testLookup.pl
+++ testLookup.pl
@@ -7,24 +7,34 @@ use Beyonwiz::WizPnP;
 
 $| = 1;
 
-die "Usage: $0 script|direct delay repeats\n"
-    if(@ARGV != 3 && ($ARGV[0] ne 'script' || $ARGV[0] ne 'direct'));
+die "Usage: $0 script|direct delay repeats [maxdevs [expectdevs [wizpnpTimeout [wizpnpPoll]]]]\n"
+    if(@ARGV < 3 || @ARGV > 7 || $ARGV[0] ne 'script' && $ARGV[0] ne 'direct');
 
-my ($how, $delay, $repeats) = @ARGV;
+my ($how, $delay, $repeats, $maxdevs, $expectdevs, $wizpnpTimeout, $wizpnpPoll) = @ARGV;
+
+$maxdevs = 1 if(!defined $maxdevs);
+$expectdevs = $maxdevs if(!defined $expectdevs);
 
 foreach my $n (1..$repeats) {
     sleep($delay);
-    print ($n % 10 == 0 ? "$n\n" : '.');
+    print ($n % 50 == 0 ? "$n\n" : '.');
     if($how eq 'script') {
-	system './doLookup.pl', $n;
+	my @args = ($n, $maxdevs, $expectdevs);
+	push @args, $wizpnpTimeout if(defined $wizpnpTimeout);
+	push @args, $wizpnpPoll if(defined $wizpnpPoll);
+	system './doLookup.pl', @args;
     } else {
 	my $pnp = Beyonwiz::WizPnP->new;
 	#Beyonwiz::WizPnP::debug(1);
 
-	$pnp->maxDevs(1);
+	$pnp->maxDevs($maxdevs);
+	$pnp->wizpnpTimeout($wizpnpTimeout) if(defined $wizpnpTimeout);
+	$pnp->wizpnpPoll($wizpnpPoll) if(defined $wizpnpPoll);
 
 	$pnp->search;
-	print "\nSearch $n failed\n" if($pnp->ndevices == 0);
+	print "\nSearch $n found ", $pnp->ndevices, " of $expectdevs devices\n"
+	    if($expectdevs == 0 && $pnp->ndevices == 0
+	    || $expectdevs != 0 && $pnp->ndevices < $expectdevs);
     }
 }
-print "\n" if($repeats % 10 != 0);
+print "$repeats\n" if($repeats % 50 != 0);
