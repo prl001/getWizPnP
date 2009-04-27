b6778daaf1870099d4a410a5224c22b3b93e5bf6
prl <prl@cae3c895-535b-42a1-8135-9dd7b94a49ff>
2009-04-27 08:37:37 +0000

    Check that the modules used or required in a list of perl script files are available on the system.

---
+++ checkModules.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use File::Spec::Functions qw(splitdir);
+
+my %checkedModule;
+my %optional = (
+    'IO::Socket::Multicast'
+	=> 'you will need to use --host to connect to your Beyonwiz devices',
+    'IO::Interface::Simple' =>
+	=> '--longNames will use IP addresses instead'
+	 . ' of the shorter host addresses'
+);
+
+sub moduleName($) {
+    my ($file) = @_;
+    $file =~ s/\.pm$//;
+    my @module = splitdir($file);
+    return join '::', @module;
+}
+
+sub addLocalModule($) {
+    my ($file) = @_;
+    $checkedModule{moduleName($file)} = 1;
+}
+
+sub checkRequire(*$$) {
+    my ($module, $type, $inFile) = @_;
+    return 1 if($checkedModule{$module});
+    $checkedModule{$module} = 1;
+    eval "require $module";
+    my $ok = 1;
+    if($@) {
+	chomp $@;
+	$ok = 0;
+	if($@ =~ /^Can't locate /) {
+	    $@ =~ s///;
+	    $@ =~ s/ in \@INC.*//;
+	    my $module = moduleName($@);
+	    $@ = 'Can\'t locate ' . $module . ' in file ' . $inFile . "\n";
+	    if($type eq 'Beyonwiz::Utils::tryUse') {
+		$@ .= "    $module is optional";
+		$@ .= ", but\n    $optional{$module}" if($optional{$module});
+		$ok = 1;
+	    } else {
+		$@ .= "    $module is required";
+	    }
+	}
+	warn "$@\n";
+    }
+    return $ok;
+}
+
+sub process($) {
+    my ($file) = @_;
+    if(!open F, '<', $file) {
+	warn "Can't open $file: $!\n";
+	return 0;
+    }
+    my $errors;
+    while(<F>) {
+	chomp;
+	if(/^\s*(require|use)\s+([A-Za-z_][A-Za-z0-9_:]*).*;\s*(#.*)?$/
+	|| /^.*(Beyonwiz::Utils::tryUse)\s+([A-Za-z_][A-Za-z0-9_:]*).*;\s*(#.*)?$/) {
+	    $errors++ if(!checkRequire $2, $1, $file);
+	}
+    }
+    return !$errors;
+}
+
+my $error;
+
+@ARGV = map { glob $_ } @ARGV if($^O eq 'MSWin32');
+
+foreach my $file (@ARGV) {
+    addLocalModule $file if($file =~ /\.pm$/);
+}
+
+foreach my $file (@ARGV) {
+    $error++ if(!process $file);
+}
+
+exit($error ? 1 : 0)
